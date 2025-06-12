534cbdb565bea6337cdb5a2cbcbf919e20580309
satakshi003 <soumya3955@gmail.com>
2025-06-12 12:24:01 +0530

    Update button changes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -38,8 +38,11 @@ cartSummaryHTML += `
               <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
               </span>
-              <span class="update-quantity-link link-primary">
+              <span class="update-quantity-link link-primary  js-update-link" data-product-id="${matchingProducts.id}">
                 Update
+              </span>
+              <input class="quantity-input">
+              <span class="save-quantity-link link-primary">Save
               </span>
               <span class="delete-quantity-link link-primary  js-delete-link"data-product-id="${matchingProducts.id}" >
                 Delete
@@ -116,3 +119,13 @@ function updateCartQuantity(){
 document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
 }
 updateCartQuantity();
+
+document.querySelectorAll('.js-update-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+   
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.classList.add('is-editing-quantity');
+
+  });
+});
