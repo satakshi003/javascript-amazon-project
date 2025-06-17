ae7917ddd6e5a52e50108d74a4269911fbecb122
satakshi003 <soumya3955@gmail.com>
2025-06-17 21:31:38 +0530

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId)
 {
   let matchingProducts;
@@ -9,6 +11,31 @@ export function getProduct(productId)
   });
 
   return matchingProducts;
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+  return  `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+  return  `$${formatCurrency(this.priceCents)}`;
+  }
+
 }
 
 export const products = [
@@ -691,4 +718,7 @@ export const products = [
     },
     priceCents: 2999
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
+
