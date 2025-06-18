0cd427974a1959afd00031e27fe47794c548591a
satakshi003 <soumya3955@gmail.com>
2025-06-18 12:57:08 +0530

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -60,6 +60,12 @@ constructor(productDetails) {
   }
 }
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
