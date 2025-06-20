7dcbcc0c8fd71cebbaf19ccdabd7a5ffd069d6e1
satakshi003 <soumya3955@gmail.com>
2025-06-20 12:02:47 +0530

    Updating code so that delivery dates are not on weekend.

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliv
 import {products, getProduct} from '../../data/products.js';
 import { formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOption.js';
+import {deliveryOptions, getDeliveryOption, calculateDeliveryDate} from '../../data/deliveryOption.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHeader.js';
 
@@ -22,13 +22,7 @@ export function renderOrderSummary(){
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
    
-
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-
-    const dateString = deliveryDate.format('dddd, MMMM D');
-
-
+const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
 
@@ -85,10 +79,8 @@ export function renderOrderSummary(){
 
 
       deliveryOptions.forEach((deliveryOption) => {
-        const today = dayjs();
-        const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-
-        const dateString = deliveryDate.format('dddd, MMMM D');
+        
+const dateString = calculateDeliveryDate(deliveryOption);
 
         const priceString = deliveryOption.priceCents === 0
         ? 'FREE'
