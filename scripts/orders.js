31be5d8560f8148df9c8f12ce87057a929e8b2d9
satakshi003 <soumya3955@gmail.com>
2025-06-21 13:31:40 +0530

    Adding a pop-up, for Buy it again button.

--- scripts/orders.js
+++ scripts/orders.js
@@ -2,6 +2,8 @@ import { getProduct, loadProducts, loadProductsFetch } from "../data/products.js
 import { orders } from "../data/orders.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { formatCurrency } from "./utils/money.js";
+import { addToCart } from "../data/cart.js";
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 
 async function loadPage() {
   await loadProductsFetch();
@@ -33,7 +35,7 @@ async function loadPage() {
           </div>
 
           <div class="order-details-grid">
-          ${productsListHTML(order)}
+          ${ productsListHTML(order)}
             </div>
           </div>
     `;
@@ -45,7 +47,7 @@ async function loadPage() {
       const product = getProduct(productDetails.productId);
 
 
-      productsListHTML += `
+      productsHTML += `
        <div class="product-image-container">
               <img src="${product.image}">
             </div>
@@ -60,7 +62,8 @@ async function loadPage() {
               <div class="product-quantity">
                 Quantity:${productDetails.quantity}
               </div>
-              <button class="buy-again-button button-primary">
+              <button class="buy-again-button button-primary js-buy-again"
+              data-product-id = "${product.id}">
                 <img class="buy-again-icon" src="images/icons/buy-again.png">
                 <span class="buy-again-message">Buy it again</span>
               </button>
@@ -76,9 +79,35 @@ async function loadPage() {
       
       `;
     });
-    return productsListHTML;
+    return productsHTML;
       }
 document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
+
+
+document.querySelectorAll('.js-buy-again').forEach((button) => {
+  button.addEventListener('click', () => {
+    addToCart(button.dataset.productId);
+
+  renderCheckoutHeader();  
+  button.innerHTML = '✅ Added to Cart';
+  button.disabled = true;
+
+  const toast = document.querySelector('.js-toast');
+  toast.classList.add('show');
+
+  setTimeout(() => {
+    toast.classList.remove('show');
+  }, 1500);
+
+ // button.innerHTML = 'Added';
+  setTimeout(() => {
+    button.innerHTML = `
+     <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+    `;
+    }, 1000);
+    });
+});
 }
 
 loadPage();
